64d0c6053936f2c31fca8d178354b7d3e83221da
MindEase Developer <154737367+MalinkaKAW@users.noreply.github.com>
2025-11-23 20:32:05 +0530

    Change tips icon from tips.svg to meditation.svg

--- MindEase/src/assets/images/index.ts
+++ MindEase/src/assets/images/index.ts
@@ -3,7 +3,7 @@ export const images = {
   meditation: require('./meditation.svg'),
   heart: require('./heart.svg'),
   breathing: require('./breathing.svg'),
-  tips: require('./tips.svg'),
+  tips: require('./meditation.svg'),
   favorites: require('./favorites.svg'),
   profile: require('./image.png'),
 };
